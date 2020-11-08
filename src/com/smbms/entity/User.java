87232a2190b25c74bb66716b903a2a43fa566383
lenovo <3255077502@qq.com>
2020-11-08 20:08:23 +0800

    第六次提交

---
+++ src/com/smbms/entity/User.java
@@ -0,0 +1,47 @@
+package com.sample;
+
+
+public class User {
+
+  private long userid;
+  private String username;
+  private long consumption;
+  private long sum;
+
+
+  public long getUserid() {
+    return userid;
+  }
+
+  public void setUserid(long userid) {
+    this.userid = userid;
+  }
+
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+
+  public long getConsumption() {
+    return consumption;
+  }
+
+  public void setConsumption(long consumption) {
+    this.consumption = consumption;
+  }
+
+
+  public long getSum() {
+    return sum;
+  }
+
+  public void setSum(long sum) {
+    this.sum = sum;
+  }
+
+}
